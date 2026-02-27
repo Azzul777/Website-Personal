fad4a55dfa04598af7bd8d013aa431d7a04255d6
Pandu <syaftianpandu@gmail.com>
2026-02-27 15:42:43 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,14 @@
+const namaSaya = 'pandu';
+let usia = 18;
+
+function generateBiodata() {
+    if ( usia > 120 && usia < 19){
+        console.log("asu")
+    }
+    else{
+        console.log("mandi")
+    }
+    console.log(`Nama saya ${namaSaya} dan usia saya ${usia} tahun `);
+}
+
+generateBiodata();
